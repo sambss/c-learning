cdc750cb809323647f37376d55243572cb319626
Samuel Barbosa <samuelbarbosaba@gmail.com>
2024-11-16 16:58:21 -0300

    feat: another exercise to predict the result from scanf

---
+++ chap_03/exercise_05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    float x, y;
+
+    scanf("%f%d%f", &x, &i, &y);
+
+    printf("x: %f\n", x);
+    printf("i: %d\n", i);
+    printf("y: %f\n", y);
+
+    return 0;
+}
