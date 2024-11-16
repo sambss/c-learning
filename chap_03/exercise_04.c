b75e6a52fb4cb42432d6cb48fbc3148f8f0c666c
Samuel Barbosa <samuelbarbosaba@gmail.com>
2024-11-16 16:55:49 -0300

    feat: adding predict scanf result

---
+++ chap_03/exercise_04.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    float x;
+
+    scanf("%d%f%d", &i, &x, &j);
+
+    printf("i: %d\n", i);
+    printf("x: %f\n", x);
+    printf("j: %d\n", j);
+
+    return 0;
+}
