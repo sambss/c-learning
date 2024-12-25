e04c8ce81c60911e7ae2ae03588691620c26fe53
Samuel Barbosa <samuelbarbosaba@gmail.com>
2024-12-25 17:06:57 -0300

    feat: script to sum a series of double values

---
+++ chap_07/7_3_project.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    double n, sum = 0;
+
+    printf("This program sums a series of Doubles.\n");
+    printf("Enter double (0 to terminate): ");
+
+    scanf("%lf", &n);
+    while (n != 0.0){
+        sum += n;
+        scanf("%lf", &n);
+    }
+
+    printf("The sum is: %lf\n", sum);
+
+    return 0;
+}
