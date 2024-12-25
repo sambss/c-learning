64b69b555c86627b3d4376a5cf95beb4a00ed34a
Samuel Barbosa <samuelbarbosaba@gmail.com>
2024-12-25 19:52:41 -0300

    feat: script to print the size of variables type

---
+++ chap_07/7_6_project.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    
+    printf("size of int: %ld\n", sizeof(int));
+    printf("size of short: %ld\n", sizeof(short));
+    printf("size of long: %ld\n", sizeof(long));
+    printf("size of float: %ld\n", sizeof(float));
+    printf("size of double: %ld\n", sizeof(double));
+    printf("size of long double: %ld\n", sizeof(long double));
+
+    return 0;
+}
