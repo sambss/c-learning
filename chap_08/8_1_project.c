aae486918c219be055c924c400953f1817e261b4
Samuel Barbosa <samuelbarbosaba@gmail.com>
2025-01-03 11:51:08 -0300

    feat: show which numbers if any were repeated

---
+++ chap_08/8_1_project.c
@@ -0,0 +1,40 @@
+/* Checks numbers for repeated digits */
+#include <stdbool.h>
+#include <stdio.h> 
+
+int main(void)
+{
+    bool digit_seen[10] = {false};
+    bool repeated_value[10] = {false};
+    bool has_repeated = false;
+    int digit;
+    long n;
+    
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while (n > 0) {
+        digit = n % 10;
+        if (digit_seen[digit]){
+            repeated_value[digit] = true;
+            has_repeated = true;
+        }
+        digit_seen[digit] = true;
+        n /= 10;
+    }
+
+    if (has_repeated){
+        printf("Repeated digit (s): ");
+        for(int i = 0; i < 10; i++){
+            if(repeated_value[i]){
+                printf("%i ", i);
+            }
+        }
+        printf("\n");
+    }
+    else {
+        printf("No repeated digit\n");
+    }
+    
+    return 0;
+}
