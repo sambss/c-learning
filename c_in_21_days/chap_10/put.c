3e736dbd52e181ce045782b8ea8966f498578925
Samuel Barbosa <samuelbarbosaba@gmail.com>
2025-01-26 18:08:45 -0300

    feat: demonstrates displaying strings with puts

---
+++ c_in_21_days/chap_10/put.c
@@ -0,0 +1,22 @@
+/* Demonstrates displaying strings with puts(). */
+#include <stdio.h>
+
+int main( void )
+{
+
+    char *message1 = "C";
+    char *message2 = "is the";
+    char *message3 = "best";
+    char *message4 = "programming";
+    char *message5 = "language!!";
+    char *strong = "Better, Faster, Harder, Stronger!";
+
+    puts(message1);
+    puts(message2);
+    puts(message3);
+    puts(message4);
+    puts(message5);
+    puts(strong);
+    
+    return 0;
+}
